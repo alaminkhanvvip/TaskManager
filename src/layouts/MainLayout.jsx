313690fa96598ac859232ddcec33b3af19125a39
alaminkhanvvip <alaminkhan.vvip@gmail.com>
2025-10-27 20:57:27 +0600

    project setup

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="h-96"></div>
+      <Footer />
+    </div>
+  );
+};
+export default MainLayout;
